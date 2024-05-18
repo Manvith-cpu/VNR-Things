e8f07c354c4f047efea325778864c128e2827db5
Manvith <biualways123@gmail.com>
2024-05-18 10:01:50 +0530

    create post api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,7 +1,8 @@
 import  express from 'express';
 import {verifyToken}from '../utils/verifyUser.js'
+import { create } from '../controllers/post.controller.js';
 
-const router=express.router();
+const router=express.Router();
 
 router.post('/create',verifyToken,create);
 
